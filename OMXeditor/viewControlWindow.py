1552b21472da94df323253b2698907ec92e57fab
Carnë Draug <carandraug+dev@gmail.com>
2015-04-03 22:41:12 +0100

    viewControlWindow: remove trailing whitespace

--- OMXeditor/viewControlWindow.py
+++ OMXeditor/viewControlWindow.py
@@ -5,7 +5,7 @@ import datadoc
 
 
 ## The ViewControlWindow class provides a window that allows the user
-# to control and customize various views of the data -- by setting the 
+# to control and customize various views of the data -- by setting the
 # 4D crosshairs, toggling visibility of a specific perspective, and creating
 # projections across orthogonal axes.
 class ViewControlWindow(wx.Frame):
@@ -20,15 +20,15 @@ class ViewControlWindow(wx.Frame):
 
         sizer = wx.BoxSizer(wx.VERTICAL)
 
-        ## List of options that could be a bit expensive (see 
+        ## List of options that could be a bit expensive (see
         # self.onAlignChange).
         self.expensiveProjectionOptions = []
         ## List of options that could be very expensive (ditto).
         self.veryExpensiveProjectionOptions = []
 
-        # Set up viewer controls in a grid. Figure out which axes pairs 
-        # are valid (i.e. both axes have a size greater than 1) and 
-        # only include them. 
+        # Set up viewer controls in a grid. Figure out which axes pairs
+        # are valid (i.e. both axes have a size greater than 1) and
+        # only include them.
         viewSizer = wx.GridSizer(2, 3, 1, 1)
         validAxes = []
         # XY, XZ, YZ, XT, YT, ZT. The first three are also the default views
@@ -58,17 +58,17 @@ class ViewControlWindow(wx.Frame):
 
 
     ## Create a panel that provides view options for a single viewer: whether
-    # or not the viewer is visible, and which, if any, axis to project the 
+    # or not the viewer is visible, and which, if any, axis to project the
     # viewer across.
     def makeViewerPanel(self, axes, isVisibleByDefault):
         panel = wx.Panel(self, -1, style = wx.BORDER_SUNKEN | wx.TAB_TRAVERSAL)
         sizer = wx.BoxSizer(wx.VERTICAL)
 
-        label = "%s-%s" % (datadoc.DIMENSION_LABELS[axes[0]], 
+        label = "%s-%s" % (datadoc.DIMENSION_LABELS[axes[0]],
                 datadoc.DIMENSION_LABELS[axes[1]])
         sizer.Add(wx.StaticText(panel, -1, label), 0, wx.ALIGN_CENTER_HORIZONTAL)
-        
-        # Size the toggle button to ensure that we fill the entire width of 
+
+        # Size the toggle button to ensure that we fill the entire width of
         # the window, assuming we have 3 columns of viewer panels.
         toggleButton = wx.ToggleButton(panel, -1, 'Visible', size = (78, -1))
         toggleButton.Bind(wx.EVT_TOGGLEBUTTON, lambda event: self.parent.toggleWindowVisibility(axes))
@@ -78,15 +78,15 @@ class ViewControlWindow(wx.Frame):
         sizer.Add(wx.StaticText(panel, -1, 'Project:'))
         baseRadio = wx.RadioButton(panel, -1, 'None', style = wx.RB_GROUP)
         baseRadio.SetValue(True)
-        baseRadio.Bind(wx.EVT_RADIOBUTTON, 
+        baseRadio.Bind(wx.EVT_RADIOBUTTON,
                 lambda event: self.parent.setViewProjection(axes, None))
         sizer.Add(baseRadio)
         # Allow projection across time, Z, Y, and X, but of course we leave
         # out any axes that are already part of the view.
         for axis in xrange(4, 0, -1):
             if axis not in axes and self.dataDoc.size[axis] > 1:
-                radio = wx.RadioButton(panel, -1, 
-                        datadoc.DIMENSION_LABELS[axis], 
+                radio = wx.RadioButton(panel, -1,
+                        datadoc.DIMENSION_LABELS[axis],
                         style = wx.RB_SINGLE)
                 if axis == 1:
                     # Time axis; could be very expensive
@@ -94,7 +94,7 @@ class ViewControlWindow(wx.Frame):
                 elif axis in [3, 4]:
                     # X/Y axis; could be moderately expensive
                     self.expensiveProjectionOptions.append(radio)
-                radio.Bind(wx.EVT_RADIOBUTTON, 
+                radio.Bind(wx.EVT_RADIOBUTTON,
                         lambda event, axis = axis: self.parent.setViewProjection(axes, axis))
                 sizer.Add(radio)
 
@@ -106,37 +106,37 @@ class ViewControlWindow(wx.Frame):
     # one axis of the image. Insert the slider, and its label, into the
     # provided sizer.
     def makeSliderControl(self, sizer, axis):
-        sizer.Add(wx.StaticText(self, -1, 
-            "%s:" % datadoc.DIMENSION_LABELS[axis]), 
+        sizer.Add(wx.StaticText(self, -1,
+            "%s:" % datadoc.DIMENSION_LABELS[axis]),
                 0, wx.ALL, 5)
-        
+
         slider = wx.Slider(self, -1, 0, 0, self.dataDoc.size[axis] - 1,
                 size = (200, -1),
                 style = wx.SL_HORIZONTAL | wx.SL_LABELS | wx.SL_AUTOTICKS)
         slider.SetValue(self.parent.getViewAxisIndex(axis))
-        slider.Bind(wx.EVT_SLIDER, 
+        slider.Bind(wx.EVT_SLIDER,
                 lambda event: self.parent.setSliceLine(axis, event.GetInt()))
         sizer.Add(slider)
         return slider
 
 
     ## Recognize that alignment parameters have changed. If there is rotation
-    # or scaling, then projections through X or Y require transforming the 
-    # entire volume, and projections through time require transforming 
-    # the entire dataset -- ouch! If this happens, we want to notify the 
+    # or scaling, then projections through X or Y require transforming the
+    # entire volume, and projections through time require transforming
+    # the entire dataset -- ouch! If this happens, we want to notify the
     # user before they click on anything, so we adjust background colors
     # and tooltips to suit.
     def onAlignChange(self, alignParams):
-        isExpensive = (numpy.any(alignParams[:,3] != 0) or 
+        isExpensive = (numpy.any(alignParams[:,3] != 0) or
                 numpy.any(alignParams[:,4] != 1))
         preamble = "Due to the rotation and scaling alignment parameters, " + \
                 "this projection option will require transforming the " + \
                 "entire "
         badColors = [(255, 255, 0), (255, 0, 0)]
-        badTips = [preamble + "current volume, which will take some time.", 
+        badTips = [preamble + "current volume, which will take some time.",
                 preamble + "entire dataset, which can take a very long time."]
-        
-        for i, radioSet in enumerate([self.expensiveProjectionOptions, 
+
+        for i, radioSet in enumerate([self.expensiveProjectionOptions,
                 self.veryExpensiveProjectionOptions]):
             for radio in radioSet:
                 if isExpensive:
@@ -145,8 +145,6 @@ class ViewControlWindow(wx.Frame):
                 else:
                     radio.SetBackgroundColour([230] * 3)
                     radio.SetToolTipString('')
-            
-
 
 
     ## Update our sliders because the user changed the view without using them.
@@ -155,4 +153,3 @@ class ViewControlWindow(wx.Frame):
         for axis, slider in self.axisToSliderMap.iteritems():
             if axis in newPositions:
                 slider.SetValue(newPositions[axis])
-
